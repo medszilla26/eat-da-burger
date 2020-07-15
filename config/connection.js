7981710613e1126b55d5c1222360944f2dc2553e
M3D5 <pablohdzjr@gmail.com>
2020-07-15 15:01:12 -0500

    added JAWSDB connection

--- config/connection.js
+++ config/connection.js
@@ -1,19 +1,22 @@
 var mysql = require("mysql2");
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "Honda126!",
-  database: "burgers_db",
-});
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "Honda126!",
+    database: "burgers_db",
+  });
 
-connection.connect(function (err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
-});
-
+  connection.connect(function (err) {
+    if (err) {
+      console.error("error connecting: " + err.stack);
+      return;
+    }
+    console.log("connected as id " + connection.threadId);
+  });
+}
 module.exports = connection;
